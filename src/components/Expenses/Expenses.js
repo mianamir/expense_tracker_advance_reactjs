8ab1eadc2b54d45752d4b98ba81a891418e4cf99
Muhammad Amir <63117404+amirsavvy4ever@users.noreply.github.com>
2021-12-08 22:26:58 +0500

    conditional rendering in the react components
    conditional rendering in the react components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,21 +15,35 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    
+    return expense.date.getFullYear().toString() === filteredYear;
+
+  });
+
+  let expenseContent = <p>No expenses found!</p>
+
+  if (filteredExpenses.length > 0){
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+      key={expense.id} 
+      title={expense.title} 
+      amount={expense.amount}
+      date={expense.date}
+      />
+      ))
+  };
+
   return (
   
       <Card className="expenses">
         <ExpensesFilter 
         selected={filteredYear} 
-        onChangeHandler={filterChangeHandler} />
-
-        {props.items.map((expense) => (
-        <ExpenseItem
-        key={expense.id} 
-        title={expense.title} 
-        amount={expense.amount}
-        date={expense.date}
+        onChangeHandler={filterChangeHandler}
         />
-        ))}
+        
+        {expenseContent}
+        
         
       </Card>
     );
